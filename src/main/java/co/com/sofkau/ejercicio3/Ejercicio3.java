b1baec2d3f59edfb309738617484b783c9e0a378
SantiagoOspinaO <santiago.m200@outlook.es>
2022-07-13 22:28:36 -0500

    Update project

--- src/main/java/co/com/sofkau/ejercicio3/Ejercicio3.java
+++ src/main/java/co/com/sofkau/ejercicio3/Ejercicio3.java
@@ -7,20 +7,19 @@ public class Ejercicio3 {
 
     public static void derivar(Integer coeficiente, Integer exponente) {
         BiFunction<Integer, Integer, Integer> coeficien = (coeficient, exponent) -> coeficient * exponent;
-        Function<Integer, Integer> exponen = (expone) -> expone - 1;
-        System.out.println("∂f/∂x " + coeficiente +"x^" + exponente +" = " + coeficien.apply(coeficiente, exponente) +"x^" + exponen.apply(exponente));
+        Function<Integer, Integer> resta = (expone) -> expone - 1;
+        System.out.println("∂f/∂x " + coeficiente +"x^" + exponente +" = " + coeficien.apply(coeficiente, exponente) +"x^" + resta.apply(exponente));
     }
 
     public static void integrar(Integer coeficiente, Integer exponente) {
         BiFunction<Integer, Integer, Integer> coeficien = (coeficient, exponent) -> {
-            if (coeficient / (exponent + 1) == 1) {
-                return 1;
-            } else {
+            if (coeficient / (exponent + 1) != 1) {
                 return coeficient / (exponent + 1);
-            }
+            } else return 1;
         };
-        Function<Integer, Integer> exponen = (expone) -> expone + 1;
-        System.out.println("∫" + coeficiente + "x^" + exponente + "∂x = " + coeficien.apply(coeficiente, exponente) + "x^" + exponen.apply(exponente) + " + c");
+        Function<Integer, Integer> suma = (expone) -> expone + 1;
+
+        System.out.println("∫" + coeficiente + "x^" + exponente + "∂x = " + coeficien.apply(coeficiente, exponente) + "x^" + suma.apply(exponente) + " + c");
     }
 
     public static void main(String[] args) {
